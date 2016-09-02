8ab4c2bccbae9be7be5b30081c4bb1d2eda2e20d
Xu hailin <384698202@qq.com>
2016-09-02 09:59:14 +0800

    Revert "no message"
    This reverts commit bfbedcdcce4755fc35006efda14e42e0c3a5033f.

---
+++ uIButtonClick1.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class uIButtonClick : MonoBehaviour {
+
+
+	public  void QuitButtonClick()
+	{ 
+		Application.Quit ();
+	}
+
+	public   void StartButtonClick()
+	{ 
+		Application.LoadLevel ("Untitled");
+	}
+	
+	//这是注释,fenzhi111,2 3
+
+	//zenmhuishi
+
+	//ai
+}
